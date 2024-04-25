ffe74851f7cefc47546900fd56faf5f394aae9b6
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-04-25 11:31:20 +0200

    modelo chat y modelo user

---
+++ Back/Prueba de nodemailer -ServerBasico/src/api/models/Chat.model.js
@@ -0,0 +1,31 @@
+// 1º requerimos mongoose y traemos la parte de los esquemas de datos
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//! --------- creamos los esquemas de datos
+
+// Definir el modelo de datos:
+// ------------> Le damos a cada clave del objeto el Type (tipo de dato)
+// ------------> definimos otras propiedades que limitan la informacion que se puede incluir en esa clave
+// ------------> que sea requerido, una longitud maxima y minima, etc etc
+
+const ChatSchema = new Schema(
+  {
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Menssage" }],
+    userOne: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    userTwo: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    /* un array de muchos mensajes por eso va entre [], entre dos usuarios, 
+    por eso los usuarios uno y dos van solo con {}, porque son un único objeto*/
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//! -------- con la definicion de datos y su esquema vamos a crear el modelo de datos
+
+const Chat = mongoose.model("Chat", ChatSchema);
+
+//! -------- exportar el modelo para que lo utilicen los controladores
+
+module.exports = Chat;
