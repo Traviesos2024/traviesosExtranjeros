03d9fbef83d430de3889104fd6395142cf9bcd3e
Gisl4 <lopgiz25@gmail.com>
2024-05-15 20:37:36 +0200

    eliminacion de data y modificacion de events

--- FrontEnd/src/pages/Eventspages.jsx
+++ FrontEnd/src/pages/Eventspages.jsx
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import { useParams } from 'react-router-dom';
-// import { eventsData } from '../data/eventsData';
-
-// const EventsPages = () => {
-//   const { city } = useParams();
-//   console.log(useParams());
-
-//   // Verifica si city está definido
-//   if (!city) {
-//     return <h2>Ciudad no definida</h2>;
-//   }
-
-//   // Encuentra el evento correspondiente
-//   const event = eventsData.find(event => event.city.toLowerCase() === city.toLowerCase());
-
-//   if (!event) {
-//     return <h2>Evento no encontrado</h2>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>{event.name}</h1>
-//       <img src={`/images/${event.image}`} alt={event.name} width="400" />
-//       <p>Ciudad: {event.city}</p>
-//       <p>Categoría: {event.category}</p>
-//       <a href="/">Volver a la lista de eventos</a>
-//     </div>
-//   );
-// };
-
-// export default EventsPages;
-
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import "./Register.css";
