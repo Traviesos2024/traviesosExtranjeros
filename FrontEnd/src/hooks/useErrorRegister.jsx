e1b6d2add12654923088cc5ef64263c13bafd1b4
Leticia Lorenzo <leticialorenzogr@outlook.es>
2024-05-19 13:29:40 +0200

    botones de select para country y city en register y arreglos back

--- FrontEnd/src/hooks/useErrorRegister.jsx
+++ FrontEnd/src/hooks/useErrorRegister.jsx
@@ -1,61 +1,67 @@
 import Swal from "sweetalert2/dist/sweetalert2.all.js";
 
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_CONFLICT = 409;
+const HTTP_STATUS_VALIDATION_ERROR = 422; // Cambia esto al código de estado correcto si es diferente
+const HTTP_STATUS_SERVER_ERROR = 500;
+
 export const useErrorRegister = (res, setRes, setOk) => {
-  //!--- si la respuesta es ok --> res.status
-  //!--- el error ---> res.response.status
+  if (res) {
+    if (res.status === HTTP_STATUS_OK) {
+      //* guardo la data en el localStorage
+      const dataToString = JSON.stringify(res);
+      localStorage.setItem("data", dataToString);
 
-  // status 200 : todo ok
-  if (res?.status == 200) {
-    //* guardo la data en el localStorage
-    const dataToString = JSON.stringify(res);
-    localStorage.setItem("data", dataToString);
+      //* setear el register ok
+      setOk(true);
 
-    //* setear el register ok
-    setOk(() => true);
+      //* lanzo swal
+      Swal.fire({
+        title: "Registro hecho!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000,
+      });
 
-    //* lanzo swal
-    Swal.fire({
-      title: "Registro hecho!",
-      icon: "success",
-      showConfirmButton: false,
-      timer: 2000,
-    });
+      //*  seteo la respuesta al estado inicial
+      setRes({});
+    } else if (res.response) {
+      if (res.response.status === HTTP_STATUS_CONFLICT) {
+        Swal.fire({
+          title: "El email no es válido",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else if (
+        res.response.status === HTTP_STATUS_VALIDATION_ERROR &&
+        res.response.data?.includes("validation failed: password")
+      ) {
+        Swal.fire({
+          title:
+            "Min 8 caracteres, 1 minúscula, 1 mayúscula, 1 número y 1 caracter especial",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      } else if (res.response.status === HTTP_STATUS_SERVER_ERROR) {
+        Swal.fire({
+          title: "internal server error 500, inténtalo de nuevo",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      } else {
+        // Manejo de cualquier otro error no anticipado
+        Swal.fire({
+          title: `Error ${res.response.status}: ${res.response.statusText || 'Ocurrió un error'}`,
+          icon: "error",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      }
 
-    //*  seteo la respuesta al estado inicial
-    setRes({});
-  }
-
-  // status 409 : user ya registrado
-  if (res?.response?.status == 409) {
-    Swal.fire({
-      title: "El email no es válido",
-      icon: "error",
-      showConfirmButton: false,
-      timer: 2000,
-    });
-    setRes({});
-  }
-
-  // status 404 : contraseña formato erroneo
-  if (res?.response?.data?.includes("validation failed: password")) {
-    Swal.fire({
-      title:
-        "Min 8 caracteres, 1 minúscula, 1 mayúscula, 1 número y 1 caracter especial",
-      icon: "error",
-      showConfirmButton: false,
-      timer: 3000,
-    });
-    setRes({});
-  }
-
-  // status 500 : internal server error
-  if (res?.response?.status == 500) {
-    Swal.fire({
-      title: "internal server error 500, inténtalo de nuevo",
-      icon: "error",
-      showConfirmButton: false,
-      timer: 2000,
-    });
-    setRes({});
+      setRes({});
+    }
   }
 };
