7eb82c57511e3e1d75039dce94c42750660a4e81
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-05-20 17:15:17 +0200

    update profile user actualizado a falta de probar

--- FrontEnd/src/pages/UpdateUserProfile.jsx
+++ FrontEnd/src/pages/UpdateUserProfile.jsx
@@ -2,7 +2,7 @@ import Swal from "sweetalert2/dist/sweetalert2.all.js";
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useUpdateError } from "../hooks";
-import { Uploadfile } from "../components";
+import { FigureUser, Uploadfile } from "../components";
 import { byId, updateUser } from "../services/user.service";
 import { useAuth } from "../context/authContext";
 import { fetchCountries, fetchCities } from "../services/user.service";
@@ -12,27 +12,30 @@ export const UpdateProfile = () => {
 
   const [resEdit, setResEdit] = useState({});
   const [sendEdit, setSendEdit] = useState(false);
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
   //! --------------- hooks
 
-  const { user, logout } = useAuth();
+  const { user, setUser, logout } = useAuth();
   const { handleSubmit, register, watch } = useForm();
   const [countries, setCountries] = useState(user?.country);
   const [cities, setCities] = useState(user?.city);
-  // const [interests, setInterests] = useState(user?.interests)
 
-  const fetchData = async () => {
-    const dataForState = await byId(user?._id);
-    setData(dataForState);
-    setIsDataReady(true);
+  const defaultData = {
+    name: user?.user,
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // const fetchData = async () => {
+  //   const dataForState = await byId(user?._id);
+  //   setData(dataForState);
+  //   setIsDataReady(true);
+  // };
 
-  const editProfileFormSubmit = async (formData) => {
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  const formSubmit = async (formData) => {
     Swal.fire({
       title: "Save changes?",
       icon: "warning",
@@ -68,7 +71,7 @@ export const UpdateProfile = () => {
   //! ----- useEffect
 
   useEffect(() => {
-    useUpdateError(resEdit, setResEdit, logout);
+    useUpdateError(resEdit, setResEdit, setUser, logout);
   }, [resEdit]);
 
   //! 5) cargar países al montar el componente
@@ -95,55 +98,56 @@ export const UpdateProfile = () => {
 
   return (
     <>
-      {data && (
-        <>
-          <input width={"40vw"} onSubmit={handleSubmit(editProfileFormSubmit)}>
-            <h1>Edit your information </h1>
-            <div className="country_container form-group">
+      <div className="containerProfile">
+        <div className="containerDataNoChange">
+          <FigureUser user={user} />
+        </div>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <h1>Edit your information </h1>
+          <div className="user_container form-group">
+            <select
+              className="input_user"
+              id="country"
+              name="country"
+              {...register("country", { required: true })}
+            >
+              <option value="">Select a country</option>
+              {countries.map((country) => (
+                <option key={country._id} value={country._id}>
+                  {country.name}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="country" className="custom-placeholder">
+              Country
+            </label>
+          </div>
+          {selectedCountry && (
+            <div className="city_container form-group">
               <select
                 className="input_user"
-                id="country"
-                name="country"
-                {...register("country", { required: true })}
+                id="city"
+                name="city"
+                country=""
+                {...register("city", { required: true })}
               >
-                <option value="">Select a country</option>
-                {countries.map((country) => (
-                  <option key={country._id} value={country._id}>
-                    {country.name}
+                <option value="">Select a city</option>
+                {cities.map((city) => (
+                  <option key={city._id} value={city._id}>
+                    {city.name}
                   </option>
                 ))}
               </select>
-              <label htmlFor="country" className="custom-placeholder">
-                Country
+              <label htmlFor="city" className="custom-placeholder">
+                City
               </label>
             </div>
-            {selectedCountry && (
-              <div className="city_container form-group">
-                <select
-                  className="input_user"
-                  id="city"
-                  name="city"
-                  country=""
-                  {...register("city", { required: true })}
-                >
-                  <option value="">Select a city</option>
-                  {cities.map((city) => (
-                    <option key={city._id} value={city._id}>
-                      {city.name}
-                    </option>
-                  ))}
-                </select>
-                <label htmlFor="city" className="custom-placeholder">
-                  City
-                </label>
-              </div>
-            )}
-            <div>
-              <Uploadfile />
-            </div>
-            //! comentado para la descripción del usuario por si queremos
-            meterlo
-            {/* <LabelAndInput inputHeight={"60px"}>
+          )}
+          <div>
+            <Uploadfile />
+          </div>
+          //! comentado para la descripción del usuario por si queremos meterlo
+          {/* <LabelAndInput inputHeight={"60px"}>
                 Brief description of you
                 <textarea
                   type="text"
@@ -153,17 +157,20 @@ export const UpdateProfile = () => {
                   {...register("description")}
                 />
               </LabelAndInput> */}
+          <div className="btn_container">
             <button
+              className="btn"
               type="submit"
-              width={"70%"}
-              disabled={sendEdit}
-              variant={sendEdit ? "loading" : "normal"}
+              disabled={send}
+              style={{ background: send ? "#49c1a388" : "#49c1a2" }}
             >
               SAVE CHANGES
             </button>
-          </input>
-        </>
-      )}
+          </div>
+        </form>
+      </div>
     </>
+    //   )}
+    // </>
   );
 };
