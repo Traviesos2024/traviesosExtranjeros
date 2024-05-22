4b328d41272afc63064b1b9cc1baa287a62f160a
NekyyM <nekyy_@hotmail.com>
2024-05-22 17:04:37 +0200

    BotonesNAV cambian con user

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -1,22 +1,59 @@
 import "./Nav.css";
+import { useAuth } from "../context/authContext";
 import { NavLink } from "react-router-dom";
 export const Nav = () => {
+  const { user, login, logout } = useAuth();
   return (
     <>
-      <nav className="navPrincipal">
-        <NavLink to="/">
-          <button>Home</button>
-        </NavLink>
-        <NavLink to="/profile">
-          <button>Profile</button>
-        </NavLink>
-        <NavLink to="/country">
-          <button>Country</button>
-        </NavLink>
-        <NavLink to="/chats">
-          <button>Chats</button>
-        </NavLink>
-      </nav>
+      {user ? (
+        <>
+          <NavLink to="/">
+            <button>Home</button>
+          </NavLink>
+          <NavLink to="/Oxford/6648e20eec90657fef4a88ee">
+            {/* Hay que hacer un link dinámico aqui para que pinte la ciudad del
+            usuario según la que corresponda */}
+            <button>Mi ciudad</button>
+          </NavLink>
+          <NavLink to="/country">
+            <button>Paises</button>
+          </NavLink>
+          <NavLink to="/chats">
+            <button>Chats</button>
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink to="/">
+            <button>Home</button>
+          </NavLink>
+          <NavLink to="/country">
+            <button>Paises</button>
+          </NavLink>
+          <NavLink to="/events">
+            <button>Eventos</button>
+          </NavLink>
+        </>
+      )}
     </>
   );
 };
+
+{
+  /* <>
+  <nav className="navPrincipal">
+    <NavLink to="/">
+      <button>Home</button>
+    </NavLink>
+    <NavLink to="/profile">
+      <button>Profile</button>
+    </NavLink>
+    <NavLink to="/country">
+      <button>Country</button>
+    </NavLink>
+    <NavLink to="/chats">
+      <button>Chats</button>
+    </NavLink>
+  </nav>
+</>; */
+}
