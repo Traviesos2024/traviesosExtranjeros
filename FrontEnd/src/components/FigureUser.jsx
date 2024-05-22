e45e482ea222de3cedf533b60830f4b75cb9887b
Elena1118 <elenaherrerafernandez1196@gmail.com>
2024-05-22 16:48:47 +0200

    profile casi casi

--- FrontEnd/src/components/FigureUser.jsx
+++ FrontEnd/src/components/FigureUser.jsx
@@ -4,6 +4,11 @@ export const FigureUser = ({ user }) => {
     <figure className="dataProfile">
       <img src={user.image} alt="user image" className="imageUser" />
       <h4 className="emailUser">Email: {user.email}</h4>
+      <h4 className="nameUser">Nombre: {user.name}</h4>
+      <h4 className="genderUser">Género: {user.gender}</h4>
+      {/* <h4 type="date">Edad: {user.age}</h4>
+      <h4 className="countryUser">País: {user?.country}</h4>
+      <h4 className="cityUser">Ciudad: {user?.city}</h4> */}
     </figure>
   );
 };
