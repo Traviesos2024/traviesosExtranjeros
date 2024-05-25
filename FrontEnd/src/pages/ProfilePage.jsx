4ee841ccb5f673c02abac598f78bcebab0552b9a
Leticia Lorenzo <leticialorenzogr@outlook.es>
2024-05-25 13:05:53 +0200

    profile ok

--- FrontEnd/src/pages/ProfilePage.jsx
+++ FrontEnd/src/pages/ProfilePage.jsx
@@ -20,9 +20,8 @@ import { useErrorEvent } from "../hooks/useErrorEvent";
 import { getAll } from "../services/events.service";
 import { Event } from "../components";
 
-export const ProfilePage = ({ items }) => {
+export const ProfilePage = ({ item }) => {
   const { user } = useAuth();
-  console.log(user);
   const [events, setEvents] = useState([]);
   const [res, setRes] = useState({});
 
@@ -40,42 +39,42 @@ export const ProfilePage = ({ items }) => {
     console.log(events);
   }, [events]);
 
+  // Filtrar los eventos que están en la lista de likeEvent del usuario
+  const likedEvents = events.filter(event => event.likeEvent.includes(user._id));
+
   return (
     <>
       <NavProfile />
       <main>
         <div className="homePage">
           <h3 className="TituloViajeros">
-            ¡¡Hola {user.user}, aquí tienes todo tu contenido!!
+            ¡¡Hola {user.name}, aquí tienes todo tu contenido!!
           </h3>
           <h2 className="EventosHome">Eventos en {user.city.name}</h2>
           <div>
-            {user.eventsFav && events.length > 0 ? (
-              events.likeEvent
-                .includes(user._id)
-                .map((item) => (
-                  <Event
-                    item={item}
-                    src={item?.image}
-                    name={item?.name}
-                    key={item._id}
-                    category={item?.category}
-                    date={item?.date}
-                    description={item?.description}
-                    cities={item?.cities?.map((city) => city.name)}
-                    eventId={item?._id}
-                    comments={item?.comments}
-                    setEvents={setEvents}
-                  />
-                ))
+            {likedEvents.length > 0 ? (
+              likedEvents.map((item) => (
+                <Event
+                  item={item}
+                  src={item?.image}
+                  name={item?.name}
+                  key={item._id}
+                  category={item?.category}
+                  date={item?.date}
+                  description={item?.description}
+                  cities={item?.cities?.map((city) => city.name)}
+                  eventId={item?._id}
+                  comments={item?.comments}
+                  setEvents={setEvents}
+                />
+              ))
             ) : (
               <p>No hay eventos favoritos disponibles</p>
             )}
           </div>
 
           <p className="parrafo">
-            Echa un vistazo de los nuevos eventos disponibles en{" "}
-            {user.city.name}
+            Echa un vistazo de los nuevos eventos disponibles en {user.city.name}
           </p>
           <button>
             <Link to="/events">Eventos</Link>
@@ -85,3 +84,4 @@ export const ProfilePage = ({ items }) => {
     </>
   );
 };
+
