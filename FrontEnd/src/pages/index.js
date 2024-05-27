f0e678a3dce829862c862fcde281a2433882aee1
Elena1118 <elenaherrerafernandez1196@gmail.com>
2024-05-27 16:36:35 +0200

    error

--- FrontEnd/src/pages/index.js
+++ FrontEnd/src/pages/index.js
@@ -19,8 +19,5 @@ export * from "./UpdateUserProfile";
 export * from "./CountryAll";
 export * from "./EventDetallePage";
 export * from "./Followers";
-<<<<<<< HEAD
 export * from "./UpdateExperience";
-=======
 export * from "./UserAll";
->>>>>>> 5abe2223422c870d761d076399981a0d289be77e
