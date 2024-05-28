ca7281907b3f38fa46a82c3d5fea7a321f889e08
Gisl4 <lopgiz25@gmail.com>
2024-05-28 15:27:39 +0200

    delete profile event

--- FrontEnd/src/components/Event.jsx
+++ FrontEnd/src/components/Event.jsx
@@ -1,10 +1,182 @@
+// import "./Event.css";
+// import { Comments } from "./index";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { toggleFollowEvent, toggleLikeEvent } from "../services/events.service"; // Importar la función de servicio
+// // import { useErrorLikeEvent, useErrorFollowEvent } from "../hooks"; // Importa un hook personalizado para manejar errores
+// import { useAuth } from "../context/authContext"; // Importa el contexto de autenticación
+// import { Link } from "react-router-dom";
+// import { followUserToggle } from "../services/user.service";
+
+// export const Event = ({
+//   name,
+//   src,
+//   category,
+//   cities,
+//   setUserById,
+//   date,
+//   description,
+//   eventId,
+//   comments,
+//   setEvents,
+//   item,
+//   initialLikes,
+//   initialFollowers,
+//   eventOwner,
+//   userById,
+// }) => {
+//   const [open, setOpen] = useState(false);
+//   const { allUser, setAllUser, bridgeData, user } = useAuth();
+//   const [likes, setLikes] = useState(initialLikes); // Estado de likes
+//   const [followed, setFollowed] = useState(initialFollowers); // follow y unfollow
+
+//   // Recuperar el estado del local storage cuando el componente se monte
+
+//   const onToggleLike = async (event) => {
+//     try {
+//       const res = await toggleLikeEvent(eventId);
+//       console.log("res", res);
+//       res.status == 200 && setEvents(res.data.allEvent);
+//     } catch (error) {
+//       console.error("Error toggling like:", error);
+//     }
+//   };
+
+//   const onToggleFollow = async (event) => {
+//     try {
+//       const res = await toggleFollowEvent(eventId);
+//       console.log("res", res);
+//       res.status == 200 && setEvents(res.data.allEvent);
+//     } catch (error) {
+//       console.error("Error toggling like:", error);
+//     }
+//   };
+
+//   // //! para seguir al usuario
+//   const onToggleFollowUser = async (idUserSeQuiereSeguir) => {
+//     console.log("entro");
+//     try {
+//       const res = await followUserToggle(idUserSeQuiereSeguir);
+//       console.log("res", res);
+//       res.status == 200 && setUserById(res.data.authUser);
+//     } catch (error) {
+//       console.error("Error toggling follow:", error);
+//     }
+//   };
+
+//   const onToggleComments = (event) => {
+//     event.preventDefault();
+//     setOpen(!open);
+//   };
+
+//   return (
+//     <figure key={eventId} className="card">
+//       <img src={src} alt={name} width={350} height={200} />
+//       <div className="card-padding">
+//         <p>{likes}</p>
+//         <div className="card-icons-wrapper">
+//           <div onClick={onToggleLike} id="favorite-icon">
+//             <span
+//               className={
+//                 item.likeEvent.includes(user._id)
+//                   ? "material-symbols-outlined favorite"
+//                   : "material-symbols-outlined"
+//               }
+//             >
+//               favorite
+//             </span>
+//             <span>{item.likeEvent.length}</span>
+//           </div>
+//           <p>{followed}</p>
+//           <div onClick={onToggleFollow} className="Check">
+//             <span
+//               className={
+//                 item.eventFollowers.includes(user._id)
+//                   ? "material-symbols-outlined Check_box"
+//                   : "material-symbols-outlined"
+//               }
+//             >
+//               Check_box
+//             </span>
+//             <span>{item.eventFollowers.length}</span>
+//           </div>
+//           <div className="comment-icon-padding">
+//             <span
+//               className="material-symbols-outlined"
+//               onClick={onToggleComments}
+//             >
+//               mode_comment
+//             </span>
+//           </div>
+//           <div>
+//             <button>
+//               <Link to="/experiences">Ver experiencias</Link>
+//             </button>
+//           </div>
+//         </div>
+//         {console.log("eventOwner", cities)}
+//         <p>Evento: {name}</p>
+//         <p>Categoría: {category}</p>
+//         <p>Fecha: {new Date(date).toLocaleString()}</p>
+//         <p>Descripción: {description}</p>
+//         <p>Ciudad: {cities?.name}</p>
+//         <div>
+//           <div></div>
+
+//           <p>
+//             Organizador:{" "}
+//             <span
+//               className={
+//                 userById?.followed?.includes(user._id)
+//                   ? "material-symbols-outlined person_add"
+//                   : "material-symbols-outlined"
+//               }
+//             >
+//               {eventOwner}
+//             </span>
+//           </p>
+
+//           <span
+//             className={
+//               userById?.followed?.includes(user._id)
+//                 ? "material-symbols-outlined person_add"
+//                 : "material-symbols-outlined"
+//             }
+//             onClick={() => onToggleFollowUser(item.eventOwner._id)}
+//           >
+//             person_add
+//           </span>
+//         </div>
+
+//         <div className="card-comments-wrapper">
+//           {open ? (
+//             <>
+//               <Comments selectedRecipient={eventId} commentsProps={comments} />
+//               <div className="close-chat-wrapper">
+//                 {/* <span>{item.comments.length}</span> */}
+//                 <span
+//                   onClick={onToggleComments}
+//                   className="material-symbols-outlined"
+//                 >
+//                   close
+//                 </span>
+//               </div>
+//             </>
+//           ) : (
+//             ""
+//           )}
+//         </div>
+//       </div>
+//     </figure>
+//   );
+// };
+
+
 import "./Event.css";
 import { Comments } from "./index";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { toggleFollowEvent, toggleLikeEvent } from "../services/events.service"; // Importar la función de servicio
-// import { useErrorLikeEvent, useErrorFollowEvent } from "../hooks"; // Importa un hook personalizado para manejar errores
-import { useAuth } from "../context/authContext"; // Importa el contexto de autenticación
+import { useState } from "react";
+import { toggleFollowEvent, toggleLikeEvent } from "../services/events.service";
+import { useAuth } from "../context/authContext";
 import { Link } from "react-router-dom";
 import { followUserToggle } from "../services/user.service";
 
@@ -24,48 +196,41 @@ export const Event = ({
   initialFollowers,
   eventOwner,
   userById,
+  handleDelete, 
 }) => {
   const [open, setOpen] = useState(false);
-  const { allUser, setAllUser, bridgeData, user } = useAuth();
-  const [likes, setLikes] = useState(initialLikes); // Estado de likes
-  const [followed, setFollowed] = useState(initialFollowers); // follow y unfollow
+  const { user } = useAuth();
+  const [likes, setLikes] = useState(initialLikes);
+  const [followed, setFollowed] = useState(initialFollowers);
 
-  // Recuperar el estado del local storage cuando el componente se monte
-
-  const onToggleLike = async (event) => {
+  const onToggleLike = async () => {
     try {
       const res = await toggleLikeEvent(eventId);
-      console.log("res", res);
-      res.status == 200 && setEvents(res.data.allEvent);
+      res.status === 200 && setEvents(res.data.allEvent);
     } catch (error) {
       console.error("Error toggling like:", error);
     }
   };
 
-  const onToggleFollow = async (event) => {
+  const onToggleFollow = async () => {
     try {
       const res = await toggleFollowEvent(eventId);
-      console.log("res", res);
-      res.status == 200 && setEvents(res.data.allEvent);
-    } catch (error) {
-      console.error("Error toggling like:", error);
-    }
-  };
-
-  // //! para seguir al usuario
-  const onToggleFollowUser = async (idUserSeQuiereSeguir) => {
-    console.log("entro");
-    try {
-      const res = await followUserToggle(idUserSeQuiereSeguir);
-      console.log("res", res);
-      res.status == 200 && setUserById(res.data.authUser);
+      res.status === 200 && setEvents(res.data.allEvent);
     } catch (error) {
       console.error("Error toggling follow:", error);
     }
   };
 
-  const onToggleComments = (event) => {
-    event.preventDefault();
+  const onToggleFollowUser = async (idUserSeQuiereSeguir) => {
+    try {
+      const res = await followUserToggle(idUserSeQuiereSeguir);
+      res.status === 200 && setUserById(res.data.authUser);
+    } catch (error) {
+      console.error("Error toggling follow:", error);
+    }
+  };
+
+  const onToggleComments = () => {
     setOpen(!open);
   };
 
@@ -113,16 +278,20 @@ export const Event = ({
               <Link to="/experiences">Ver experiencias</Link>
             </button>
           </div>
+          {handleDelete && ( 
+            <button onClick={() => handleDelete(eventId)}>
+              <span class="material-symbols-outlined">
+                delete
+              </span>
+            </button>
+          )}
         </div>
-        {console.log("eventOwner", cities)}
         <p>Evento: {name}</p>
         <p>Categoría: {category}</p>
         <p>Fecha: {new Date(date).toLocaleString()}</p>
         <p>Descripción: {description}</p>
         <p>Ciudad: {cities?.name}</p>
         <div>
-          <div></div>
-
           <p>
             Organizador:{" "}
             <span
@@ -135,7 +304,6 @@ export const Event = ({
               {eventOwner}
             </span>
           </p>
-
           <span
             className={
               userById?.followed?.includes(user._id)
@@ -147,13 +315,11 @@ export const Event = ({
             person_add
           </span>
         </div>
-
         <div className="card-comments-wrapper">
           {open ? (
             <>
               <Comments selectedRecipient={eventId} commentsProps={comments} />
               <div className="close-chat-wrapper">
-                {/* <span>{item.comments.length}</span> */}
                 <span
                   onClick={onToggleComments}
                   className="material-symbols-outlined"
