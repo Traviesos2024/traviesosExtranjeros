2aee1f31d9b2ae1ee4c363b81846fc742697a20d
Elena1118 <elenaherrerafernandez1196@gmail.com>
2024-06-03 19:40:19 +0200

    css y country

--- FrontEnd/src/components/CardCountryHome.jsx
+++ FrontEnd/src/components/CardCountryHome.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import "./CardCountryHome.css";
+
 export const CardCountryHome = ({ data }) => {
-  const { image, name } = data;
+  const { image, name, country} = data;
   const path = `/country/${data.name}/${data._id}`;
   return (
     <div className="cardCountryHome">
       <Link to={path}>
+      <h3> {name}</h3>
+        <h3>{country}</h3>
         <img src={image} alt={name} />
       </Link>
     </div>
